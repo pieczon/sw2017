c0b81c6ff8e061d6dc967cab5fdcb4ec72237bf7
pieczon <bandarhawaii@gmail.com>
2017-11-02 10:12:02 +0100

    add

--- algorytmy-lista.c
+++ algorytmy-lista.c
@@ -9,30 +9,33 @@ struct studtoku
  struct studtoku *adrnast;
 
 }studtoku;
-//sara={3, 24658, "Kosmetologia", "Dawidziuk"};
+
+struct studtoku sara={3, 24658, "Kosmetologia", "Dawidziuk", NULL};
+int t = 7;
 
 int main()
 {
 //2. inicjalizacja listy, tworzenie obiektów po kolei: początek listy, element listy
+
 struct studtoku *head;
 struct studtoku *kolejny;
-
 head = malloc(sizeof(studtoku));
 kolejny = malloc(sizeof(studtoku));
-
 head->adrnast = NULL;
 
 //3. zapełnianie listy 
+
 kolejny = kolejny->adrnast = malloc(sizeof(studtoku));
 kolejny->adrnast = NULL;
 
-kolejny->rokst = 4;
-kolejny->nralb = 25555;
-kolejny->kierunek[20] = "Lekarski";
-kolejny->nazwisko[20] = "Pokora";
-//kolejny-> {4, 39727, "Stomatologia", "Przydrożna"};
+//kolejny->rokst = 4;
+//kolejny->nralb = 25555;
+//kolejny->kierunek[] = "Lekarski";
+//kolejny->nazwisko[] = "Pokora";
+//kolejny-> struct studtoku aaa={4, 39727, "Stomatologia", "Przydrożna"};
 
 //4. pokazywanie listy
+
 while(kolejny != NULL)
 {
 printf("Dane studentów, rok studiów: %d, numer albumu: %d, nazwa kierunku: %s, nazwisko: %s \n", kolejny->rokst, kolejny->nralb, kolejny->kierunek, kolejny->nazwisko);
