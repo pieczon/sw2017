87cccdd66e55f528dc229f0fce2c08515de8ed32
pieczon <31313385+pieczon@users.noreply.github.com>
2017-11-24 01:05:58 +0100

    Update zadanie8.2.3.c

--- zadanie8.2.3.c
+++ zadanie8.2.3.c
@@ -3,19 +3,27 @@
 
 //8.2.3 (r,!) Napisz funkcję, która dostaje jako argument ścieżkę dostępu do pliku tekstowego i wypisuje na standardowym wyjściu zawartość pliku z pominięciem białych znaków. Napisz dwie wersje funkcji dla znaków typów char i wchar_t.
 
+void wypisz(char *sciezka)
+{
+    FILE *id = fopen(sciezka, "r");
+    char c;
+    printf("\n----------------------------------------------------------------\n");
+    printf("Deskryptor: %p\nNazwa: %s\n", id, sciezka);
+    printf("Zawartość bez spacji: \n");
+    printf("----------------------------------------------------------------\n");
+    while(fscanf(id,"%c", &c) == 1)
+    {
+        if(!isspace(c))
+        {
+            printf("%c", c);
+        }
+    }
+    printf("\n");
+    fclose(id);
+}
 int main()
 {
-    void wypisz(char *sciezka)
-    {
-        FILE *id = fopen(sciezka, "r");
-        char c;
-        while(fscanf(plik, "%c", &c)) ==1)
-        {
-            if (!isspace(c))
-            {
-                printf("%c", c);
-            }
-        }
-        fclose(plik);
-    }    
+    wypisz("liczba.c");
+    wypisz("/home/pieczon/PzESW/sw2017/coldwind-przyklady_na_pointerach.c");
+    return 0;
 }
