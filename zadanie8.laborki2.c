acfd79dc35a6ca644e906e2e894bb433e8e11d32
pieczon <31313385+pieczon@users.noreply.github.com>
2017-11-26 20:21:10 +0100

    Update zadanie8.laborki2.c

--- zadanie8.laborki2.c
+++ zadanie8.laborki2.c
@@ -1,41 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Program ma zapisać do drugiego pliku, te linijki z otwartego pliku, zawierające cyfry
+
 #define N 99
-//Program ma zapisać do drugiego pliku linijki otwartego pliku zawierające cyfry
-char buff[100] = {0};
+char buff[1000] = {0};
 
-
-
-// void zcyframi(char *sciezka, char *zapis)
-// {
-//     char c;
-//     printf("Deskryptor: %p\nPlik odczytu: %s\n", fr, sciezka);
-//     printf("Plik zapisu: %s\n", zapis);
-//     int znak;
-// 	int tab[256] = {0};
-// 	while((znak = fgetc(fr)) != EOF)
-// 	{
-// 		tab[znak] = tab[znak] + 1;
-//     }
-//     while(fscanf(fr,"%c", &c) == 1)
-//     {
-//         for(int i=0; i<256; i++)
-//         {
-//             if(tab[i]>0 && (i>=48 && i<=57))
-//             {
-//                 fprintf(fw, "%c", c);
-//             }
-//         }
-//     }
-//     printf("\n");
-//     fclose(fw);
-//     fclose(fr);
-// }
 int liniazawieracyfre(char *l)
 {
     int znaleziono = 0;
-    while(0!=*l) //sprawdzamy czy wsk nie wskazuje na zero, po lewej stronie wartość nieprzepisywalna, bo pod nia nie mozna nic wpisac, wiec warunek bedzie dobrze zapisany, zapis rownowazny (*l==0)
+    while(0!=*l) //sprawdzamy czy wsk nie wskazuje na zero, po lewej stronie wartość nieprzepisywalna, bo pod nia nie mozna nic przypisac, zapis rownowazny (*l==0)?
     {
         if(isdigit(l))
         {
@@ -49,14 +23,15 @@ int main()
 {
     FILE *fr = fopen("plikdoodczytu", "r");
     FILE *fw = fopen("plikdozapisu", "w");
+
     while(!feof(fr))
     {
         fgets(buff, N, fr);
         if(liniazawieracyfre(buff))
         {
-        fputs(buff, fw);
-        fclose(fw);
-        fclose(fr);
+            fputs(buff, fw);
+            fclose(fw);
+            fclose(fr);
         }
     }
     return 0;
