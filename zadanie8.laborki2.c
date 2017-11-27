f576242d14ba307ce40da6338d0fb9cffa152a13
pieczon <31313385+pieczon@users.noreply.github.com>
2017-11-27 10:28:25 +0100

    Update zadanie8.laborki2.c

--- zadanie8.laborki2.c
+++ zadanie8.laborki2.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 //Program ma zapisać do drugiego pliku, te linijki z otwartego pliku, zawierające cyfry
 
-#define N 99
-char buff[1000] = {0};
+#define N 200
+char buff[200] = {0};
 
 int liniazawieracyfre(char *l)
 {
     int znaleziono = 0;
-    while(0!=*l) //sprawdzamy czy wsk nie wskazuje na zero, po lewej stronie wartość nieprzepisywalna, bo pod nia nie mozna nic przypisac, zapis rownowazny (*l==0)?
+    while(0!=*l) //(0!=*l) sprawdzamy czy wsk nie wskazuje na zero, po lewej stronie wartość nieprzepisywalna, bo pod nia nie mozna nic przypisac, zapis rownowazny (*l==0)?
     {
-        if(isdigit(l))
+        if(isdigit(*l))
         {
-            znaleziono++; //sprawdzamy tylko czy mamy cyfre w linijsce
+            znaleziono++; //sprawdzamy tylko czy mamy cyfre w linijce
         }
         l++; //przesuwamy sie wskaznikiem, niezaleznie od znalezienia
     }
@@ -23,16 +24,15 @@ int main()
 {
     FILE *fr = fopen("plikdoodczytu", "r");
     FILE *fw = fopen("plikdozapisu", "w");
-
     while(!feof(fr))
     {
         fgets(buff, N, fr);
         if(liniazawieracyfre(buff))
         {
-            fputs(buff, fw);
-            fclose(fw);
-            fclose(fr);
+            fputs(buff, fw);  
         }
     }
+    fclose(fw);
+    fclose(fr);
     return 0;
 }
