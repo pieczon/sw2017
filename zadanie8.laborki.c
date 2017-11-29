4154805475bb5d304f625e737f1e957f2be4d58a
pieczon <31313385+pieczon@users.noreply.github.com>
2017-11-29 17:27:35 +0100

    Add files via upload

---
+++ zadanie8.laborki.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// wczytac liczbe z klawiatury x
+// wpisac ta liczbe do pliku x do file x-f
+// close(f)
+// open(f)
+// plik wczytac do x f-x
+// printf(x)
+
+int main()
+{
+    int x =0;
+    printf("Podaj liczbę: ");
+    scanf("%d",x);
+    
+}
