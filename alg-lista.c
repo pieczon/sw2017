c7923ae4c26873dab3b59dc3696baf6ad83bf291
pieczon <31313385+pieczon@users.noreply.github.com>
2018-01-16 19:36:44 +0100

    Update alg-lista.c

--- alg-lista.c
+++ alg-lista.c
@@ -2,87 +2,78 @@
 #include <stdlib.h>
 #include <string.h>
 
-//1. definiuję strukturę, z dodaniem przykładowych rekordów do listy
-struct studtoku
+//definicja listy dwukierunkowej z zestawem danych studentów
+typedef struct student
 {
     int rokst, nralb;
-    char *kierunek, *nazwisko;
-    struct studtoku *adrnast;
-    //struct studtoku *adrpopr;
-}studtoku;
-
-struct studtoku sara={3, 24658, "Kosmetologia", "Dawidziuk", NULL};
-struct studtoku nadia={1, 56777, "Biotechnologia", "Zalewska", NULL};
+    char *kier, *nazw;
+    struct student *next;
+    struct student *prev;
+}student;
+//utworzenie 2 rekrodów listy
+struct student sara={3, 24658, "Kosmetologia", "Dawidziuk", NULL};
+struct student nadia={1, 56777, "Biotechnologia", "Zalewska", NULL};
+struct student ania={5, 34899, "Zdrowie publiczne", "Kloczkowska", NULL};
 
 int main()
 {
-//2. inicjalizacja listy, tworzenie nowych obiektów listy: początek listy, elementy listy
-    struct studtoku *head;
-    head = NULL;
-
-    struct studtoku *kolejny;
-    kolejny = malloc(sizeof(studtoku)); 
-
-    kolejny->adrnast = NULL;
+    puts("\nLista studentów:\n");
+//inicjalizacja listy, utworzenie początku listy
+    student *head;
+    head = &sara;
+    sara.next = &nadia;
+//tworzenie elementów listy
+    student *kolejny;
+    kolejny = malloc(sizeof(student)); 
+//różne sposoby implementacji
     kolejny->rokst = 4;
     kolejny->nralb = 25555;
-    kolejny->kierunek = (char*)malloc(sizeof(char) * (strlen("Lekarski") + 1));
-    strcpy(kolejny->kierunek, "Lekarski");
-    //strcpy(kierunek, bufor);     
-    kolejny->nazwisko = (char*)malloc(sizeof(char) * (strlen("Pokora") + 1));
-    strcpy(kolejny->nazwisko, "Pokora");
-    
+    kolejny->kier = (char*)malloc(sizeof(char) * (strlen("Lekarski") + 1));
+    strcpy(kolejny->kier, "Lekarski");
+    kolejny->nazw = (char*)malloc(sizeof(char) * (strlen("Pokora") + 1));
+    strcpy(kolejny->nazw, "Pokora");
+    kolejny->next = &sara;
     head = kolejny;
-
-    kolejny->adrnast = &sara;
-    sara.adrnast = &nadia;
-
-    struct studtoku ania={5, 34899, "Zdrowie publiczne", "Kloczkowska", NULL};
-    nadia.adrnast = &ania;
-
-//3. pokazywanie listy
-    struct studtoku *tmp = NULL;
+ //kolejny rekord
+    nadia.next = &ania;
+//wyświetlanie listy, struktura pomocnicza
+    student *tmp = NULL;
     tmp = head;
     while(tmp != NULL)
     {
-        printf("  %s, nr: %d, %s, rok: %d\n", tmp->nazwisko, tmp->nralb, tmp->kierunek, tmp->rokst);
-        tmp = tmp->adrnast;
+        printf("  %s, nr: %d, %s, rok: %d\n", tmp->nazw, tmp->nralb, tmp->kier, tmp->rokst);
+        tmp = tmp->next;
     }
-    // printf("Pokora: %s\n", kolejny->nazwisko);
-    // printf("Pokora: %d\n", kolejny->nralb);
-    // printf("Pokora: %s\n", kolejny->kierunek);
-    // printf("Pokora: %d\n", kolejny->rokst);
 
-// 4. usuwanie danych z listy
-
-    struct studtoku *usun = NULL;
-    struct studtoku *przedos = NULL;
+//usuwanie danych z listy, tworzenie węzłów pomocniczych
+    struct student *usun = NULL;
+    struct student *przedos = NULL;
     usun = head;
-    while(usun->adrnast != NULL)
+    while(usun->next != NULL)
     {
         przedos = usun;
-        usun = usun->adrnast;    
+        usun = usun->next;    
     }
-    przedos->adrnast = NULL;
+    printf("\nPrzedostatni: %s", przedos->nazw);
+    printf("\nOstatni: %s\n", usun->nazw);
+    puts("Kasownie ostatniego elementu listy\n");
+    przedos->next = NULL;
     
-    printf("Przedostatni: %s\n", przedos->nazwisko);
-    printf("Ostatni: %s\n", usun->nazwisko);
+    // while(usun->next != NULL)
+    // {
+    //     if(usun->nralb != 0)
+    //     {
+    //         free(usun);
+    //     }
+    //     //usun = usun->next;    
+    // }
 
-    while(usun->adrnast != NULL)
-    {
-        if(usun->nralb == 25555)
-        {
-            free(usun);
-        }
-        //usun = usun->adrnast;    
-    }
     tmp = head;
     while(tmp != NULL)
     {
-        printf("Po skasowaniu %s, nr: %d, %s, rok: %d\n", tmp->nazwisko, tmp->nralb, tmp->kierunek, tmp->rokst);
-        tmp = tmp->adrnast;
+        printf("  %s, nr: %d, %s, rok: %d\n", tmp->nazw, tmp->nralb, tmp->kier, tmp->rokst);
+        tmp = tmp->next;
     }
-
 
 /*
 Do zrobienia jeszcze w programie:
@@ -94,5 +85,7 @@ Do zrobienia jeszcze w programie:
 6. implementacja kolejki?
 7. 
 */  
+    
+    puts("\nKoniec listy studentów.\n");
     return 0;
 }
